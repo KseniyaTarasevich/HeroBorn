4e51a3dd30b28405e314770465483881da61619b
Kseniya Tarasevich <tarasevichkseniya11@gmail.com>
2022-08-02 21:20:39 +0300

    next chapter

--- Hero Born/Assets/Scripts/PlayerBehavior.cs
+++ Hero Born/Assets/Scripts/PlayerBehavior.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
@@ -10,12 +8,29 @@ public class PlayerBehavior : MonoBehaviour
     private float vInput;
     private float hInput;
 
+    private Rigidbody _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
     void Update()
     {
         vInput = Input.GetAxis("Vertical") * moveSpeed;
         hInput = Input.GetAxis("Horizontal") * rotateSpeed;
 
-        this.transform.Translate(Vector3.forward * vInput * Time.deltaTime);
-        this.transform.Rotate(Vector3.up * hInput * Time.deltaTime);
+        //this.transform.Translate(Vector3.forward * vInput * Time.deltaTime);
+        //this.transform.Rotate(Vector3.up * hInput * Time.deltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 rotation = Vector3.up * hInput;
+
+        Quaternion angleRot = Quaternion.Euler(rotation * Time.deltaTime);
+
+        _rb.MovePosition(this.transform.position + this.transform.forward * vInput * Time.fixedDeltaTime);
+        _rb.MoveRotation(_rb.rotation * angleRot);
     }
 }
